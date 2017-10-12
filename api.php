d3d639a0577a5e82fc89c749cf2e4e5880be5e3d
nawin.c <chanaudom@gmail.com>
2017-10-12 10:08:16 +0700

    ffff
    fff

---
+++ api.php
@@ -0,0 +1,8 @@
+<?php
+$access_token = 'S/WKkJ/cr6+vqEj2bCvpy4oYGRc0PE2pH7Io97Ijf1yOllU6u0Mdu54yzxHiNMVzTlDTDD3a3BO3bBYwtwNOzU6U8pZPpqtptKZvyVSbRfX93pkWuAaSo6yublI6URZaGZkez/1eRJy3f/VW4bPeqQdB04t89/1O/w1cDnyilFU=';
+
+echo $access_token;
+
+
+
+?>
